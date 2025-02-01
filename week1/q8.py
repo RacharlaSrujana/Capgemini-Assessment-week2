5dc3a49c7a746d19fb9bf2039a93fd524f4fb11e
RacharlaSrujana <racharlasrujana@gmail.com>
2025-02-01 19:17:55 +0530

    week1

--- week1/q8.py
+++
@@ -1,6 +0,0 @@
-'''8. Multiplication Table Generator'''
-
-number=int(input("Enter the number : "))
-ran=int(input("Enter range to generate table : "))
-for i in range(1,ran+1):
-    print(f"{number} * {i} = {number*i}")
